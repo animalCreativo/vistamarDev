a8498c35d085a923ab521ec9e9d990ff62eeb641
pancho <francisco.barriosr@gmail.com>
2017-10-11 14:54:38 -0300

    correcion de idioma

--- contact.php
+++ contact.php
@@ -2,14 +2,14 @@
 	include('languages/lang.php');
 	if($_POST) {
 
-		$to = "infovistamar@grupobelen.cl"; // Your email here
+		$to = "francisco.barriosr@gmail.com"; // Your email here
 		$subject = 'Info VistaMar'; // Subject message here
 
 	}
 
 	//Send mail function
 	function send_mail($to,$subject,$message,$headers,$mensajeCorreoOk,$mensajeCorreoBad ){
-		if(@mail($to,$subject,$message,$headers)){
+		if(mail($to,$subject,$message,$headers)){
 		//	echo json_encode(array('info' => 'success', 'msg' => "Your message has been sent. Thank you!"));
 		//	echo json_encode(array('to' => $to, 'subject' => $subject, 'message' => $message,'headers' => $headers));
 
